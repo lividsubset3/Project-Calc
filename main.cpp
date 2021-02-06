ffae58b32a1fef5acccd03cfc62f856aeac46d62
Patrick Rakowicz <lividsubset3@gmail.com>
2021-02-06 02:02:30 -0700

    ++ Return to selection

--- main.cpp
+++ main.cpp
@@ -50,67 +50,73 @@ void doubl(numbers<double>& numo) {
  * But this is a little more advanced with two case values, allowing for more of a cleaner look.
  * */
 int main() {
-    cout << "Type to choose:\n\n";
-    cout << "1. integer\n";
-    cout << "2. double\n";
-    cout << "3. Exit Program\n";
+    char repeat = 'y';
+    while (repeat == 'y') {
+        cout << "Type to choose:\n\n";
+        cout << "1. integer\n";
+        cout << "2. double\n";
+        cout << "3. Exit Program\n";
 
-    int selec;
-    cout << "Selected: ";
-    cin >> selec;
+        int selec;
+        cout << "Selected: ";
+        cin >> selec;
 
-    numbers<int> num_int;       //define integer function
-    numbers<double> num_dbl;    //define double function
-    int typeOfVal = 0;          // 1 for integer, 2 for double
+        numbers<int> num_int;       //define integer function
+        numbers<double> num_dbl;    //define double function
+        int typeOfVal = 0;          // 1 for integer, 2 for double
 
-    switch(selec) { //First case, this is for the selection of integer and double values.
-        case 1:
-            integ(num_int);
-            typeOfVal = 1;
-            break;
-        case 2:
-            doubl(num_dbl);
-            typeOfVal = 2;
-            break;
-        case 3:
-            return 0;
-        default:
-            cout << "Error" << endl;
-            return 1;
+        switch(selec) { //First case, this is for the selection of integer and double values.
+            case 1:
+                integ(num_int);
+                typeOfVal = 1;
+                break;
+            case 2:
+                doubl(num_dbl);
+                typeOfVal = 2;
+                break;
+            case 3:
+                return 0;
+            default:
+                cout << "Error" << endl;
+                return 1;
+        }
+        //Type of operation + - * /
+        int typeOfOp = 0;
+        cout << "Type of operation..." << endl;
+        cout << "1. +" << endl;
+        cout << "2. -" << endl;
+        cout << "3. *" << endl;
+        cout << "4. /" << endl;
+        cin >> typeOfOp;
+
+        int reslint;        //result for int
+        double resldbl;     // result for double
+
+        switch(typeOfOp){ //second case function, this an if else value for the double and integer, just defines if its a 1 or a 2 1 being integer and 2 being double
+            case 1:
+                if (typeOfVal == 1) reslint = num_int.add(); else resldbl = num_dbl.add();
+                break;
+            case 2:
+                if (typeOfVal == 1) reslint = num_int.subtract(); else resldbl = num_dbl.subtract();
+                break;
+            case 3:
+                if (typeOfVal == 1) reslint = num_int.multiply(); else resldbl = num_dbl.multiply();
+                break;
+            case 4:
+                if (typeOfVal == 1) reslint = num_int.divide(); else resldbl = num_dbl.divide();
+            default:
+                cout << "~Error~" << endl;
+                return 1;
+        }
+        //This is defining as the final answer.
+        cout << "The answer: " << endl;
+        if (typeOfVal == 1) cout << reslint << endl;
+        else cout << resldbl << endl;
+
+        cin.get();
+
+        cout << "Back to selection? (y/n)";
+        cin >> repeat;
     }
-    //Type of operation + - * /
-    int typeOfOp = 0;
-    cout << "Type of operation..." << endl;
-    cout << "1. +" << endl;
-    cout << "2. -" << endl;
-    cout << "3. *" << endl;
-    cout << "4. /" << endl;
-    cin >> typeOfOp;
-
-    int reslint;        //result for int
-    double resldbl;     // result for double
-
-    switch(typeOfOp){ //second case function, this an if else value for the double and integer, just defines if its a 1 or a 2 1 being integer and 2 being double
-        case 1:
-            if (typeOfVal == 1) reslint = num_int.add(); else resldbl = num_dbl.add();
-            break;
-        case 2:
-            if (typeOfVal == 1) reslint = num_int.subtract(); else resldbl = num_dbl.subtract();
-            break;
-        case 3:
-            if (typeOfVal == 1) reslint = num_int.multiply(); else resldbl = num_dbl.multiply();
-            break;
-        case 4:
-            if (typeOfVal == 1) reslint = num_int.divide(); else resldbl = num_dbl.divide();
-        default:
-            cout << "~Error~" << endl;
-            return 1;
-    }
-    //This is defining as the final answer.
-    cout << "The answer: " << endl;
-    if (typeOfVal == 1) cout << reslint << endl;
-    else cout << resldbl << endl;
-
-    cin.get();
     return 0;
 }
