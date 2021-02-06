5ad9b17884452030bfb6c3eb79471a93a4733b81
Patrick Rakowicz <lividsubset3@gmail.com>
2021-02-06 01:54:48 -0700

    ++ How things work

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+//This class is defining both numbers that can be input.
+//this was a bit advanced for the first project, but I found out I could do this so i gave it a try
 template<class T>
 class numbers {
     private:
@@ -21,6 +23,7 @@ class numbers {
             {return (num2 != 0) ? num1 / num2 : 0;}
 };
 
+//This is all the values that only allow for one value, examle "1" "2"
 void integ(numbers<int>& numo) {
     int number1;
     int number2;
@@ -31,6 +34,7 @@ void integ(numbers<int>& numo) {
     numo.setvalues(number1, number2);
 }
 
+//This is for the double value, example "2.1" "4.1"
 void doubl(numbers<double>& numo) {
     double number1;
     double number2;
@@ -41,6 +45,10 @@ void doubl(numbers<double>& numo) {
     numo.setvalues(number1, number2);
 }
 
+/*
+ * This is the basic of the main class, this is defining all the values that is caused for the basic functions.
+ * But this is a little more advanced with two case values, allowing for more of a cleaner look.
+ * */
 int main() {
     cout << "Type to choose:\n\n";
     cout << "1. integer\n";
@@ -48,13 +56,14 @@ int main() {
     cout << "3. Exit Program\n";
 
     int selec;
+    cout << "Selected: ";
     cin >> selec;
 
-    numbers<int> num_int;
-    numbers<double> num_dbl;
-    int typeOfVal = 0; // 1 for integer, 2 for double
+    numbers<int> num_int;       //define integer function
+    numbers<double> num_dbl;    //define double function
+    int typeOfVal = 0;          // 1 for integer, 2 for double
 
-    switch(selec) {
+    switch(selec) { //First case, this is for the selection of integer and double values.
         case 1:
             integ(num_int);
             typeOfVal = 1;
@@ -66,7 +75,7 @@ int main() {
         case 3:
             return 0;
         default:
-            cout << "Error, Try again." << endl;
+            cout << "Error" << endl;
             return 1;
     }
 
@@ -78,10 +87,10 @@ int main() {
     cout << "4. /" << endl;
     cin >> typeOfOp;
 
-    int reslint; //result for int
-    double resldbl; // result for double
+    int reslint;        //result for int
+    double resldbl;     // result for double
 
-    switch(typeOfOp){
+    switch(typeOfOp){ //second case function, this an if else value for the double and integer, just defines if its a 1 or a 2 1 being integer and 2 being double
         case 1:
             if (typeOfVal == 1) reslint = num_int.add(); else resldbl = num_dbl.add();
             break;
@@ -97,8 +106,8 @@ int main() {
             cout << "~Error~" << endl;
             return 1;
     }
-
-    cout << "The answer:" << endl;
+    //This is defining as the final answer.
+    cout << "The answer: " << endl;
     if (typeOfVal == 1) cout << reslint << endl;
     else cout << resldbl << endl;
 
