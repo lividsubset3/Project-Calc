af36e4396e02ae488487045ca9523efa8c0a560c
Patrick Rakowicz <lividsubset3@gmail.com>
2021-02-16 02:49:05 -0700

    Removed Double, and changed stuff around

--- main.cpp
+++ main.cpp
@@ -17,25 +17,14 @@ class numbers {
             {return num1 + num2;}
         T subtract()
             {return num1 - num2;}
-        T multiply()
-            {return num1 * num2;}
         T divide()
             {return (num2 != 0) ? num1 / num2 : 0;}
+        T multiply()
+            {return num1 * num2;}
 };
 
-//This is all the values that only allow for one value, examle "1" "2"
-void integ(numbers<int>& numo) {
-    int number1;
-    int number2;
-    cout << "First number: " << endl;
-    cin >> number1;
-    cout << "Second number: " << endl;
-    cin >> number2;
-    numo.setvalues(number1, number2);
-}
-
-//This is for the double value, example "2.1" "4.1"
-void doubl(numbers<double>& numo) {
+//changing this to do both double value and integer value
+void InputNumber(numbers<double>& numo) {
     double number1;
     double number2;
     cout << "First number: " << endl;
@@ -53,28 +42,20 @@ int main() {
     char repeat = 'y';
     while (repeat == 'y') {
         cout << "Type to choose:\n\n";
-        cout << "1. integer\n";
-        cout << "2. double\n";
-        cout << "3. Exit Program\n";
+        cout << "1. Basic Math...\n";
+        cout << "3. Exit~\n";
 
         int selec;
         cout << "Selected: ";
         cin >> selec;
 
-        numbers<int> num_int;       //define integer function
-        numbers<double> num_dbl;    //define double function
-        int typeOfVal = 0;          // 1 for integer, 2 for double
+        numbers<double> numD;    //define double function
 
         switch(selec) { //First case, this is for the selection of integer and double values.
             case 1:
-                integ(num_int);
-                typeOfVal = 1;
+                InputNumber(numD);
                 break;
             case 2:
-                doubl(num_dbl);
-                typeOfVal = 2;
-                break;
-            case 3:
                 return 0;
             default:
                 cout << "Error" << endl;
@@ -89,29 +70,27 @@ int main() {
         cout << "4. /" << endl;
         cin >> typeOfOp;
 
-        int reslint;        //result for int
-        double resldbl;     // result for double
+        double reslT;     // result for double
 
         switch(typeOfOp){ //second case function, this an if else value for the double and integer, just defines if its a 1 or a 2 1 being integer and 2 being double
             case 1:
-                if (typeOfVal == 1) reslint = num_int.add(); else resldbl = num_dbl.add();
+                reslT = numD.add();
                 break;
             case 2:
-                if (typeOfVal == 1) reslint = num_int.subtract(); else resldbl = num_dbl.subtract();
+                reslT = numD.subtract();
                 break;
             case 3:
-                if (typeOfVal == 1) reslint = num_int.multiply(); else resldbl = num_dbl.multiply();
+                reslT = numD.multiply();
                 break;
             case 4:
-                if (typeOfVal == 1) reslint = num_int.divide(); else resldbl = num_dbl.divide();
+                reslT = numD.divide();
             default:
                 cout << "~Error~" << endl;
                 return 1;
         }
         //This is defining as the final answer.
         cout << "The answer: " << endl;
-        if (typeOfVal == 1) cout << reslint << endl;
-        else cout << resldbl << endl;
+        cout << reslT << endl;
 
         cin.get();
         // this is for a return back to the selection to allow for another input.
